51eb73e2dd7d2a797b380c07f173013e95f53009
kkmetech <kkmetech@gmail.com>
2025-03-02 20:08:27 +0700

    <!-- Modal Component --><!-- Table Component -->

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,56 +1,113 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <!-- Modal Component -->
+    <el-dialog
+      :visible.sync="modalVisible"
+      width="30%"
+      center
+      :show-close="true"
+      :close-on-click-modal="true"
+      :close-on-press-escape="true"
+    >
+      <div slot="title">
+        <h2>Verify that you are human</h2>
+      </div>
+      <div class="cf-turnstile" data-sitekey="0x4AAAAAAA7SN9BgICU6k8R5"></div>
+    </el-dialog>
+
+    <!-- Table Component -->
+    <el-table
+      :data="tableData"
+      style="width: 1606px"
+    >
+      <el-table-column type="expand">
+        <template>
+          <div></div>
+        </template>
+      </el-table-column>
+      <el-table-column prop="code" width="195"></el-table-column>
+      <el-table-column prop="type" width="78"></el-table-column>
+      <el-table-column prop="category" width="188"></el-table-column>
+      <el-table-column prop="number" width="157"></el-table-column>
+      <el-table-column prop="startTime" width="188"></el-table-column>
+      <el-table-column prop="duration" width="188"></el-table-column>
+      <el-table-column prop="endTime" width="188"></el-table-column>
+      <el-table-column prop="price" width="188"></el-table-column>
+      <el-table-column width="188">
+        <template>
+          <span class="grab-single" @click="openModal">แข่งขันรับงาน</span>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      modalVisible: false,
+      tableData: [
+        {
+          code: 'EA1-LAK',
+          type: 'C',
+          category: '4W',
+          number: '226',
+          startTime: '10-02-2025 12:30',
+          duration: '0h 26m',
+          endTime: '10-02-2025 13:30',
+          price: '2395'
+        },
+        {
+          code: 'EA1-AYUU',
+          type: 'C',
+          category: '4W',
+          number: '226',
+          startTime: '10-02-2025 12:30',
+          duration: '0h 26m',
+          endTime: '10-02-2025 13:30',
+          price: '2395'
+        },
+        {
+          code: 'AYUU-NO3',
+          type: 'C',
+          category: '4W',
+          number: '226',
+          startTime: '10-02-2025 12:30',
+          duration: '0h 26m',
+          endTime: '10-02-2025 13:30',
+          price: '2395'
+        }
+      ]
+    };
+  },
+  mounted() {
+    // Load Turnstile script
+    const script = document.createElement('script');
+    script.src = 'https://challenges.cloudflare.com/turnstile/v0/api.js';
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+
+    // Define the callback
+    window.onloadTurnstileCallback = (token) => {
+      console.log("Turnstile token:", token);
+      // Handle the token as needed
+    };
+  },
+  methods: {
+    openModal() {
+      this.modalVisible = true;
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+.grab-single {
+  cursor: pointer;
+  color: blue;
+  text-decoration: underline;
 }
 </style>
